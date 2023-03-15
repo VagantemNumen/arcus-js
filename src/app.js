cb2d9e1aa08dd0c54677d7746083b277fdcb25f9
Ahmed Misbah <ahmed.misbah@arabiyya.edu.mv>
2023-03-15 15:50:32 +0500

    More eslint annoyances. Bot login implementation

--- src/app.js
+++ src/app.js
@@ -1,2 +1,13 @@
 import * as dotenv from 'dotenv'
+import { Client, Events, GatewayIntentBits } from 'discord.js'
+
 dotenv.config()
+const token = process.env['DISCORD_BOT_TOKEN']
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Bot is ready! Logged in as ${c.user.tag}`)
+})
+
+client.login(token)
