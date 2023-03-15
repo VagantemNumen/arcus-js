8a780a24d0843f9401d169fa9e0ce1638b61f8bd
Ahmed Misbah <ahmed.misbah@arabiyya.edu.mv>
2023-03-15 15:36:24 +0500

    Removes eris

--- src/app.js
+++ src/app.js
@@ -4,11 +4,16 @@ import Eris from 'eris'
 dotenv.config()
 const BOT_TOKEN = process.env.DISCORD_BOT_TOKEN
 
-const bot = new Eris(BOT_TOKEN)
+const bot = new Eris(BOT_TOKEN, {
+  intents: ['guildMessages'],
+})
 
 bot.on('ready', () => console.log('Bot is ready!'))
 
+bot.on('error', (err) => console.error(err))
+
 bot.on('messageCreate', (msg) => {
+  console.log(msg)
   if (msg.content == '!ping') {
     bot.createMessage(msg.channel.id, 'Pong!')
   }
