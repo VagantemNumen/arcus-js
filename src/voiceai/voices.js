ca03ce10652d0abd018b2386e7e54d1521c18085
Ahmed Misbah <ahmed.misbah@arabiyya.edu.mv>
2023-03-16 19:45:49 +0500

    Voices array

---
+++ src/voiceai/voices.js
@@ -0,0 +1,29 @@
+export const voices = [
+  {
+    id: 'FaM07U45XKS2siN3Ip8t',
+    name: 'voice-0',
+    labels: {
+      accent: 'american',
+      age: 'young',
+      gender: 'female',
+    },
+  },
+  {
+    id: 'ehKCItPSM3amDIcp303W',
+    name: 'voice-1',
+    labels: {
+      accent: 'british',
+      age: 'young',
+      gender: 'female',
+    },
+  },
+  {
+    id: 'KG1L1mLVuWqToxsbdwce',
+    name: 'voice-2',
+    labels: {
+      accent: 'australian',
+      age: 'young',
+      gender: 'female',
+    },
+  },
+]
